4e5c4e7e3d4c5c5e6ce0b76326399337b0ee8408
阑森 <@LAPTOP-8EU808G9>
2022-04-24 16:19:54 +0800

    添加项目文件。

---
+++ agree/Program.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+class program {
+    public static void Main(string[] args) { 
+    yiqibang fg = new yiqibang();
+    fg.HelpPoint = 100;
+    fg.agree();
+    fg.disagree();
+    }
+
+}
+abstract class Yiqibang {
+    public int HelpPoint;
+    public abstract void agree();
+    public abstract void disagree();
+}
+class yiqibang : Yiqibang { 
+
+    public override void agree() {
+        Console.WriteLine(++HelpPoint);
+    }
+    public override void disagree(){
+        Console.WriteLine(--HelpPoint);
+    }
+}
+
+//interface Agree {
+//    void agree();
+
+//}
+//interface Disagree {
+//    void disagree();
+//}
