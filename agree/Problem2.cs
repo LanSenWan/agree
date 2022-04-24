a2a68972cc8cd2144babfb94376f143b35ce57a3
lansen <740998971@qq.com>
2022-04-24 22:40:48 +0800

    添加文件Suggest 删除文件Article 在Problem中添加一行内容：作业3.3

--- agree/Problem2.cs
+++ agree/Problem2.cs
@@ -7,6 +7,6 @@ using System.Threading.Tasks;
 namespace agree
 {
     internal class Class2
-    {
+    {//作业3.3
     }
 }
--- agree/Suggest.cs
+++ agree/Suggest.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace agree
 {
-    internal class Article2
+    internal class Suggest
     {
     }
 }
